7c8b2ae58e2b15757be005d8ca77c4450b7fb03e
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-04 11:41:46 -0800

    Update friendship controller

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,2 +1,15 @@
 class FriendshipController < ApplicationController
+  def create
+    new_friend_id = params[:user_id]
+    @friendship = Friendship.find_or_initialize_by(user_id: current_user.id, friend_id: new_friend_id, status: false)
+    if @friendship.save
+      flash[:notice] = 'Now you attended to this event.'
+    else
+      flash[:alert] = 'Something wrong happened'
+    end
+    redirect_to root_path
+  end
+
+  def accept_friend(user)
+  end
 end
