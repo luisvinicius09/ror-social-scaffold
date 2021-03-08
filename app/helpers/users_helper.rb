60dc44aa579535ae4a660e288790d3eeef51d51b
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-08 18:25:07 -0300

    Fix rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,13 @@
 module UsersHelper
   def request_button(current, id)
-    if current != id
-      render 'friendships/request'
-    end
+    render 'friendships/request' unless current == id
   end
 
   def friendship_link(current, id)
-    if current == id
-      link_to 'Friendship requests', friendships_path, class: 'btn'
-    end
+    link_to 'Friendship requests', friendships_path, class: 'btn' unless current != id
   end
-  
-  def friends()
-    link_to 'Friends', friends_path, class: 'btn'
+
+  def friends(current, id)
+    link_to 'Friends', friends_path, class: 'btn' unless current != id
   end
 end
