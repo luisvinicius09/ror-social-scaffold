8fd921f58a22c3d3dc51dff341a84f1cfa825260
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-08 17:41:57 -0300

    Add routes to see current friends

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   end
   resources :friendships, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'friends', to: 'friendships#friends', as: 'friends'
 end
