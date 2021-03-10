a644cb0b9be274fef24997dd37168e00bb516e6c
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-10 15:56:17 -0300

    Update methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,8 +6,4 @@ module UsersHelper
   def friendship_link(current, id)
     link_to 'Friendship requests', friendships_path, class: 'btn' unless current != id
   end
-
-  def friends(current, id)
-    link_to 'Friends', friends_path, class: 'btn' unless current != id
-  end
 end
