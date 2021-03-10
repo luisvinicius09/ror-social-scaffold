82e33ee583d35336778584fdbcef520f026b9f6a
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-10 14:55:50 -0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,13 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :inverse_friendships, class_name: 'friendship', foreign_key: 'friend_id'
+
+  has_many :friend_requests, through: :inverse_friendships
+
+  has_many :confirmed_friendships, -> { where status: true }, class_name: :friendships
+  has_many :friends, through: :confirmed_friendships
+
+  has_many :pending_friendships, -> { where status: false }, class_name: :friendships, foreign_key: :user_id
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
 end
