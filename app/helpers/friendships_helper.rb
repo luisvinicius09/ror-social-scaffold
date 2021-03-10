5d07413afb91002e635956656e78a7033c01c1f3
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-10 15:18:03 -0300

    Add method to find requests

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,7 @@
 module FriendshipsHelper
+  def who_send(fs)
+    user1 = Friendship.find_by(user_id: fs.user_id, friend_id: fs.friend_id)
+    user2 = Friendship.find_by(user_id: fs.friend_id, friend_id: fs.user_id)
+    user1.id > user2.id
+  end
 end
