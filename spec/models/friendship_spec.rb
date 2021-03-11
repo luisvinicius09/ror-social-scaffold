4c0aa2d3ee5c9bb8ea2a6dad648d273d34cc8a3f
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-11 13:48:59 -0300

    Create tests to test friendship model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -31,5 +31,15 @@ RSpec.describe Friendship, :type => :model do
 
       expect(Friendship.destroy(Friendship.first.id)).to eql(fs)
     end
+
+    it 'checks if the friendship belongs to the user' do
+      friendship = Friendship.reflect_on_association(:user)
+      expect(friendship.macro).to eql(:belongs_to)
+    end
+
+    it 'checks if the friendship belongs to the inverted user' do
+      friendship = Friendship.reflect_on_association(:friend)
+      expect(friendship.macro).to eql(:belongs_to)
+    end
   end
 end
